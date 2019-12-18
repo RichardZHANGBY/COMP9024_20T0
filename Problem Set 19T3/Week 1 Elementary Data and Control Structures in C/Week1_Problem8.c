4d4fab3f68ce8bc932cb252cbe23154584a099a3
Mushan <wwd137669793@gmail.com>
2019-12-18 11:14:03 +1100

    12.18

--- Problem Set 19T3/Week 1 Elementary Data and Control Structures in C/Week1_Problem8.c
+++ Problem Set 19T3/Week 1 Elementary Data and Control Structures in C/Week1_Problem8.c
@@ -3,9 +3,10 @@
 int max(int a, int b, int c);
 
 int main(int argc, char *argv[]) {
-	printf("The max number: %d", max(45, 813, 4));
+    printf("The max number: %d", max(45, 813, 813));
 }
 
 int max(int a, int b, int c){
-	return (a * (a > b) * (a > c) + b * (b > c) * (b > a) + c * (c > a) * (c > b));
+    return (a * (a > b) * (a > c) + b * (b > c) * (b > a) + c * (c > a) * (c > b) \
+    + a * (a == b) * (a > c) + a * (a > b) * (a == c) + c * (c > a) * (c == b));
 }
